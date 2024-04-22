9a889a3e53eb2832465f885332faa7d9e251994a
Jerome Guan <jeromelearnerg@outlook.com>
2024-04-22 14:57:08 +0800

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.next();
+        input.next
+    }
+
+    public class abc {
+        /*System.out.print("/*aaa")*/
+    }
+}
